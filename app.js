2a5afa73fef4062a83bd2d8a85bfa157efb9b1bb
Alissa <alissamak@outlook.com>
2022-03-23 20:26:59 -0400

    completed bonus and all tests

--- app.js
+++ app.js
@@ -1,5 +1,24 @@
 class Casino {
-    // Write code here
+    constructor(name, isFakeCoin){
+        this.name = name;
+        this.timesPlayed = 0;
+        this.isFakeCoin = isFakeCoin;
+    }
+    playGame(betAmount){
+        if (Math.random() <= 0.5 || this.isFakeCoin){
+            // console.log(this.name + " wins!");
+            console.log(`${this.name} wins!`);
+        }
+        else{
+            // console.log("You won " + betAmount * (this.timesPlayed + 1) + " dollars from " + this.name + "!");
+            console.log(`You won ${betAmount*(this.timesPlayed + 1)} dollars from ${this.name}!`);
+            this.timesPlayed += 1;
+        }
+    }
+    rollDie(d){
+        // console.log("You rolled a " + (Math.floor(Math.random() * d) + 1) + "!");
+        console.log(`You rolled a ${(Math.floor(Math.random() * d) + 1)}!`)
+    }
 };
 
 // TESTS
@@ -11,20 +30,18 @@ myCasino.playGame(25);
 myCasino.playGame(35);
 
 // BONUS TESTS
-/*
 const myBonusCasino = new Casino("HackerU Bonus Casino", true);
 console.log(myBonusCasino);
 myBonusCasino.playGame(5);
 myBonusCasino.playGame(15);
 myBonusCasino.playGame(25);
 myBonusCasino.playGame(35);
-*/
+
 
 // Extra BONUS TESTS
-/*
 const myExtraBonusCasino = new Casino("HackerU Extra Bonus Casino", false);
 console.log(myExtraBonusCasino);
 myExtraBonusCasino.rollDie(6);
 myExtraBonusCasino.rollDie(20);
 myExtraBonusCasino.rollDie(100);
-*/
+
